62f8f729d5468b0389071817f6538cf91935e5a3
skillzaa <skillzaa.com@gmail.com>
2022-09-06 19:52:15 +0500

    in the name of Allah

--- try.js
+++ try.js
@@ -4,34 +4,33 @@ let bil = new Bilza("bilza", 70);
 
 bil.background.color.set("#031363");
 
-const txt = cf.text("Text Padding Demo", "#ffff00");
+const comp = cf.particleSystem(20,hsl(0),20);
 /**
  * By default padding = Non-Responsive 
  * This line will make paddings reactive i.e it will 
  * take in percentage rather than numbers.
  * This line should be placed immediately after creation
  */
-txt.setRespPadding(true);
+comp.setRespPadding(true);
 
-bil.insert.add(txt, 0, 20);
+bil.insert.add(comp, 0, 20);
 
-txt.align(1,1);
-txt.x.set(50);
-txt.y.set(50);
+comp.align(1,1);
+comp.x.set(50);
+comp.y.set(50);
 
-txt.fitToWidth.set(true);
-txt.width.set(50);
+comp.width.set(5);
 
-txt.showBackground.set(true);
-txt.colorBackground.set("#000000");
+comp.showBackground.set(true);
+comp.colorBackground.set( hsl(300));
 
-txt.border.set(4);
-txt.colorBorder.set("red");
+comp.border.set(4);
+comp.colorBorder.set( hsl(60) );
 
-txt.paddingLeft.animate(2, 4, 0, 20);
-txt.paddingRight.animate(5, 7, 0, 20);
-txt.paddingTop.animate(8, 10, 0, 20);
-txt.paddingBottom.animate(11,14, 0, 20);
+comp.paddingLeft.animate(2, 4, 0, 20);
+comp.paddingRight.animate(5, 7, 0, 20);
+comp.paddingTop.animate(8, 10, 0, 20);
+comp.paddingBottom.animate(11,14, 0, 20);
 
 const ui = new Ui(bil);
 bil.draw();
